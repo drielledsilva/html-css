59b3bdf05b4f57de81f7a040cfb423b10d996c06
Drielle da Silva <drielle.silva@live.com>
2022-01-10 20:31:51 -0300

    Update scripts.js

---
+++ javascript/ESTUDOS_JAVASCRIPT/arquivos-js/12_operadores_comparacao/js/scripts.js
@@ -0,0 +1,32 @@
+var idade = 18;
+var possuiCarro = 1;
+
+if(idade >= 18) {
+    console.log("Pode tirar a carteira de habilitação!");
+}
+
+if(idade <= 17) {
+    console.log("Não pode tirar carteira de habilitação!");
+}
+
+if(possuiCarro) {
+    console.log("O usuário já pode andar de carro.");
+}
+
+var nome = "Matheus";
+
+if(nome == "Matheus") {
+    console.log("O seu nome é Matheus.");
+}
+
+if(nome != "Matheus") {
+    console.log("O nome não é Matheus");
+}
+
+if(20 > 10) {
+    console.log("Passou");
+}
+
+if(10 < 20) {
+    console.log("Passou 2");
+}
