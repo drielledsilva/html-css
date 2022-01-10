df961e7c182341b8c05cecb09b420cdc170b26ee
Drielle da Silva <drielle.silva@live.com>
2022-01-10 19:00:25 -0300

    Update scripts.js

---
+++ javascript/ESTUDOS_JAVASCRIPT/arquivos-js/10_arrays/js/scripts.js
@@ -0,0 +1,24 @@
+var arr = [5, "Matheus", true, {teste: 1, teste: 2}];
+// arrays são armazenados entre [] e objetos entre {}
+
+console.log(arr);
+
+var arr2 = [2, 4, 5, 6, 7];
+
+console.log(arr2);
+
+// acessando elementos de arrays
+
+// Arrays começam com o índice 0
+console.log(arr[1]);
+console.log(arr2[0]); 
+
+// adicionando elementos em arrays 
+arr[4] = 10;
+
+// modificando elementos de arrays 
+arr[0] = "Teste";
+
+console.log(arr);
+
+// não existe um tipo específico de array na linguagem js 
