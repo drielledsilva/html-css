e608fa9a9025dfe8b4520d58641fc38814304d8d
Drielle da Silva <drielle.silva@live.com>
2022-01-20 16:46:55 -0300

    atualizações

---
+++ javascript/ESTUDOS_JAVASCRIPT/arquivos-js/15_operadorlogico_AND(&&)/js/scripts.js
@@ -0,0 +1,20 @@
+var idade = 16;
+var nome = "João";
+
+if(nome == "João" && idade == 16) {
+    console.log("O João pode entrar na aula de violino");
+} else {
+    console.log("Este não é o João!");
+}
+
+if(1 == 1 && 3 > 2 && true) {
+    console.log("Passou");
+}
+
+/* isolando validações */ 
+if((1 == 1 && 3 > 4) && true) {
+    console.log("Passou");
+} else if(nome === "João" && idade >= 14) {
+    console.log("Aqui passa!");
+}
+
