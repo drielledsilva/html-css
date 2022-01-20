6b6ed053803f95b3134017c457392ecaa756d1a9
Drielle da Silva <drielle.silva@live.com>
2022-01-20 16:19:00 -0300

    atualizações

---
+++ javascript/ESTUDOS_JAVASCRIPT/arquivos-js/13_if_else/js/scripts.js
@@ -0,0 +1,25 @@
+var nome = "Xavier";
+
+if(nome == "Pedro") {
+    console.log("O nome dele é pedro");
+} else if(nome == "Matheus") {
+    console.log("O nome é Matheus");
+} else if(nome == "Xavier") {
+    console.log("O nome é Xavier");
+} else {
+    console.log("Ele possui outro nome!");
+}
+
+var idade = 19;
+
+if(idade > 20) {
+    console.log("Ele pode entrar na festa!");
+} else if(idade >= 18) {
+    console.log("Ele só pode entrar com autorização");
+} 
+
+if(nome == "Matheus") {
+    console.log("Teste");
+} else {
+    console.log("Testando");
+}
