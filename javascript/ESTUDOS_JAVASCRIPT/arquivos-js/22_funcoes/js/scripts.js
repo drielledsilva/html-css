51f9542cd1d81fde63984153b2d7b7f23387a5a5
Drielle da Silva <drielle.silva@live.com>
2022-01-23 20:36:46 -0300

    funções

---
+++ javascript/ESTUDOS_JAVASCRIPT/arquivos-js/22_funcoes/js/scripts.js
@@ -0,0 +1,37 @@
+function primeiraFuncao() {
+
+    console.log('Hello World das funções');
+
+}
+
+primeiraFuncao();
+
+function dizerNome(nome) {
+
+    console.log('O nome é: ' + nome);
+
+}
+
+dizerNome('Joana');
+dizerNome('Camile');
+dizerNome('Alex');
+
+let nomeDoBancoDeDados = 'Ashe';
+
+dizerNome(nomeDoBancoDeDados);
+
+function soma(a, b) {
+    let soma = a + b;
+    return soma;
+}
+
+let somaUm = soma(2, 5);
+
+console.log(somaUm);
+
+let somaDois = soma(5, 5);
+
+console.log(somaDois);
+
+console.log(soma(4, 5));
+
