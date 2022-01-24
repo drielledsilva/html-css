657e52849f5179faaf4c1f0a39f773dcb408b196
Drielle da Silva <drielle.silva@live.com>
2022-01-24 20:33:12 -0300

    métodos numéricos

---
+++ javascript/ESTUDOS_JAVASCRIPT/arquivos-js/26_metodos_numericos/js/scripts.js
@@ -0,0 +1,26 @@
+// parseFloat (números com pontos flutuantes)
+
+console.log(parseFloat('12.098'));
+console.log(Number.parseFloat('12.678'));
+
+
+// parseInt
+
+console.log(parseInt('10'));
+console.log(parseInt(18.90));
+
+
+// toFixed()
+
+console.log(23.765.toFixed(2));
+
+// isNaN
+
+console.log(isNaN('Teste'));
+console.log(isNaN(12));
+console.log(isNaN('13'));
+
+// MAX_VALUE e MIN_VALUE - acessa o maior e o menor valor que o js aceita
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
