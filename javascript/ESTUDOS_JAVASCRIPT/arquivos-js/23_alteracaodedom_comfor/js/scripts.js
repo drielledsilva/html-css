000a4823bce3dc9efddb31ee089f220be6303843
Drielle da Silva <drielle.silva@live.com>
2022-01-23 21:05:37 -0300

    Alterando o DOM

---
+++ javascript/ESTUDOS_JAVASCRIPT/arquivos-js/23_alteracaodedom_comfor/js/scripts.js
@@ -0,0 +1,22 @@
+let lista = ['Laranja', 'Uva', 'Maçã', 'Banana', 'Abacaxi'];
+
+let listaUl = document.createElement('ul');
+
+let body = document.getElementsByTagName('body');
+
+body[0].appendChild(listaUl);
+
+let listaNoBody = document.getElementsByTagName('ul');
+
+console.log(listaNoBody[0]);
+
+for(let i = 0; i < lista.length; i++) {
+
+    let liFor = document.createElement('li');
+
+    let textoLi = document.createTextNode(lista[i]);
+
+    liFor.appendChild(textoLi);
+
+    listaNoBody[0].appendChild(liFor)
+}
