c3ebe31afa6e6480c6206d3cd65271e72c3d68ca
Drielle da Silva <drielle.silva@live.com>
2022-02-02 17:16:51 -0300

    atualizações

---
+++ javascript/ESTUDOS_JAVASCRIPT/arquivos-js/41_adicionando_css_pelo_dom/js/scripts.js
@@ -0,0 +1,16 @@
+// seleciona o elemento
+var title = document.querySelector("#title");
+
+// adiciona o estilo 
+title.style.color = "red";
+
+// background-color
+title.style.backgroundColor = "yellow";
+
+// selecionar o elemento
+var subtitle = document.querySelector(".subtitle");
+
+// adicionando vários stilos
+subtitle.style.cssText = "color: blue; background-color: pink; font-size: 50px";
+
+
